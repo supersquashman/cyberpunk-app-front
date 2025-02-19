5d8e57b965111baf24ac4e89163df415d8fee290
supersquashman <supersquashman@gmail.com>
2025-02-19 00:04:06 -0600

    added basics for other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { CharacterMainComponent } from './characters/character-main/character-ma
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { SkillsComponent } from './characters/skills/skills.component';
+import { WeaponsComponent } from './characters/equipment/weapons/weapons.component';
+import { ItemsComponent } from './characters/equipment/items/items.component';
+import { ArmorComponent } from './characters/equipment/armor/armor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CharacterMainComponent,
-    SkillsComponent
+    SkillsComponent,
+    WeaponsComponent,
+    ItemsComponent,
+    ArmorComponent
   ],
   imports: [
     BrowserModule,
